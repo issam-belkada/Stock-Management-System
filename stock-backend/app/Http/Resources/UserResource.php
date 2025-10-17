1d28f3961caf5e484be0b1448d2a842e4faa6e13
issam <belkadaissam@gmail.com>
2025-10-17 23:20:41 +0100

    added resources

--- stock-backend/app/Http/Resources/UserResource.php
+++ stock-backend/app/Http/Resources/UserResource.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
+    public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'themePref' => $this->themePref,
+            'role' => $this->role ? $this->role->name : null,
+            'permissions' => $this->role 
+                ? $this->role->permissions->pluck('name') 
+                : [],
+        ];
     }
 }
